696d02475ae674de2f5f88eac5d8915c3150269c
MolhamM <32056236+MolhamM@users.noreply.github.com>
2019-10-13 07:58:30 -0700

    lockMouseFix

--- Assets/Scripts/CursorLock.cs
+++ Assets/Scripts/CursorLock.cs
@@ -4,12 +4,31 @@ using UnityEngine;
 
 public class CursorLock : MonoBehaviour
 {
-
+    [SerializeField]
+    bool InGame;
     #region MONO CALLBACKS
     void Start()
     {
+        if (InGame)
+        {
+            HideCursor();
+        }
+        else
+        {
+            ShowCursor();
+        }
+    }
+    #endregion
+    #region PRIVATE METHODS
+    void HideCursor()
+    {
         Cursor.visible = false;
         Cursor.lockState = CursorLockMode.Locked;
+    }
+    void ShowCursor()
+    {
+        Cursor.visible = true;
+        Cursor.lockState = CursorLockMode.None;
     }
     #endregion
 
